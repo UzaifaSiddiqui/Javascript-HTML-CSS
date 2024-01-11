65fc2eb91fd39af4808e33f53a360ef5ead15bf6
UzaifaSiddiqui <107631853+UzaifaSiddiqui@users.noreply.github.com>
2024-01-11 16:05:55 +0500

    Add files via upload

---
+++ 1/nine-one-twentyfour.js
@@ -0,0 +1,29 @@
+/*let num = prompt("Enter the number");
+if (num%5=== 0){
+    console.log("Multiple of 5!")
+}else{
+    console.log("Not Multiple of 5!")
+}
+// For Of loop:
+
+let n = "Uzaifa";
+for(let i of n){ //iterator -> for characters
+    console.log(i);
+}
+
+// For In loop:
+
+let students ={
+    Name : "Uzaifa",
+    age : 23,
+    institute : "UBIT"
+};
+
+for (let val in students){// iterator for objects6
+    console.log(students[val]);
+}*/
+for (let i=0 ; i<=100 ; i++){
+    if (i%2===0){
+        console.log(i);
+    }
+}
